c788d83bb0c65b811d521785c2146a50ca5137be
EmpireMTR <zio.sabayev@gmail.com>
2024-07-29 09:33:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bumper
+_A simple yet useful disguises api using reflections_
+
+- I use Reflections because I want it compatible with all versions don't judge me!
---
+++ src/main/java/xyz/atomdev/bumper/Bumper.java
@@ -0,0 +1,14 @@
+package xyz.atomdev.bumper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bumper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
